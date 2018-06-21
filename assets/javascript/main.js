3fe30d30ac7b4464a01669cbca98084040a34289
JohanBermeo <sonjohan@hotmail.com>
2018-06-21 01:52:37 -0400

    Bug with image width fixed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   var giphy = {
     gifArray: ['The Simpsons', 'Family Guy', 'American Dad', 'Futurama', 'X-men', 'Archer', 'Dragon Ball', 'King of the hill', "Bob's Burguers", 'Mighty Mouse'],
     results: [],
+    started: false,
 
     setStage: function () {
       // Generate all buttons in array
@@ -18,26 +19,25 @@ $(document).ready(function () {
       $('.gif').on('click', function (event) {
         giphy.callApi($(this).attr('data-name'));
       });
-
-      // Add Button
-      this.addButton();
-
-      //Clear Results
-      this.clearResults();
     },
 
     addButton: function () {
       $("#add-value").on('click', function (event) {
         event.preventDefault();
-
         if ($('#add-input').val().trim().length != 0) {
           giphy.gifArray.push($('#add-input').val().trim());
-          $('#buttons').empty();
           $('#add-input').val('');
-          $('#help-text').html('Enter the name of your own button.').toggleClass('text-muted text-danger');
+          $('#buttons').empty();
+          $('#help-text').html('Enter the name of your own button.');
+          if ($('#help-text').hasClass('text-danger')) {
+            $('#help-text').removeClass('text-danger').addClass('text-muted');
+          };
           giphy.setStage();
         } else {
-          $('#help-text').html('Please enter a valid name.').toggleClass('text-danger text-muted');
+          $('#help-text').html('Please enter a valid name.')
+          if ($('#help-text').hasClass('text-muted')) {
+            $('#help-text').removeClass('text-muted').addClass('text-danger');
+          };
         };
       });
     },
@@ -52,24 +52,41 @@ $(document).ready(function () {
         let arr = giphy.results;
         giphy.results = arr.concat(response.data);
         console.log(response.data);
-        for (var i = 0; i < giphy.results.length; i++) {
-          var a = $('<div class="card item float-left mr-2 mb-3">');
-          var rating = giphy.results[i].rating;
-          var b = $('<p>').text('Rating: ' + rating);
 
-          var c = $('<img>');
-          c.attr('data-state', 'still');
-          c.attr('data-animate', giphy.results[i].images.fixed_height.url);
-          c.attr('data-still', giphy.results[i].images.fixed_height_still.url)
-          c.attr('src', giphy.results[i].images.fixed_height_still.url);
-
-          a.prepend(b);
-          a.prepend(c);
-          $('#display-gifs').prepend(a);
-        };
-
+        //Render Images
+        giphy.setImages();
+        //Change on click betwen animations and stills
         giphy.changeState();
+        //Clear Results
+        giphy.clearResults();
+        // Add Button
+        giphy.addButton();
       });
+    },
+
+    setImages: function () {
+      $('#display-gifs').empty();
+      for (var i = 0; i < giphy.results.length; i++) {
+        var a = $('<div class="card item float-left mr-2 mb-3 col-md-auto col-sm-12">');
+        var rating = giphy.results[i].rating;
+        var b = $('<h5>').text('Rating: ' + rating).addClass('card-title');
+        var c = $('<div class="card-body">');
+        c.append(b)
+
+        var d = $('<img>');
+        d.addClass('img-fluid pt-3 card-img-top');
+        d.attr('data-state', 'still');
+        d.attr('data-animate', giphy.results[i].images.fixed_height.url);
+        d.attr('data-still', giphy.results[i].images.fixed_height_still.url)
+        d.attr('src', giphy.results[i].images.fixed_height_still.url);
+
+        a.append(d);
+        a.append(c);
+        
+        $('#display-gifs').prepend(a);
+      };
+
+
     },
 
     changeState: function () {
@@ -85,13 +102,16 @@ $(document).ready(function () {
       });
     },
 
-    clearResults: function(){
-      $('#clear-results').on('click', function(){
+    clearResults: function () {
+      $('#clear-results').on('click', function () {
         $('#display-gifs').empty();
         giphy.results.length = 0;
       })
     }
   };
 
-  giphy.setStage();
+  if (!giphy.started) {
+    giphy.setStage();
+    giphy.started = true;
+  }
 });
